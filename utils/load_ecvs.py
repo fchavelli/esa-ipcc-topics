b150588a8fd6b0cf044dcd493a7f4a42f455a71f
fchavelli <felchvl@gmail.com>
2024-02-19 17:56:18 +0100

    load ecvs

---
+++ utils/load_ecvs.py
@@ -0,0 +1,8 @@
+import pickle
+
+ecvs_file_path = './data/cci/ecvs.pkl'
+with open(ecvs_file_path, 'rb') as file:
+    ecv_instances = pickle.load(file)
+
+for ecv in ecv_instances:
+    ecv.display()
